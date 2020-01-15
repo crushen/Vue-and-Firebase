d9aa9175e2c5ca78d0b14bec42a6b6e2b49472a2
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-15 12:28:21 +0000

    Changes faq file name

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HomePage from '@/pages/Home';
 import AboutPage from '@/pages/About';
-import FAQPage from '@/pages/FAQ';
+import FaqPage from '@/pages/Faq';
 import LoginPage from '@/pages/Login';
 import RegisterPage from '@/pages/Register';
 
@@ -11,7 +11,7 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', name: 'HomePage', component: HomePage },
   { path: '/about', name: 'AboutPage', component: AboutPage },
-  { path: '/faq', name: 'FAQPage', component: FAQPage },
+  { path: '/faq', name: 'FaqPage', component: FaqPage },
   { path: '/login', name: 'LoginPage', component: LoginPage },
   { path: '/register', name: 'RegisterPage', component: RegisterPage }
 ]
