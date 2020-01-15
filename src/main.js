8c93e5d4b5df03cfb767a1f062afd73c67881424
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-15 11:59:32 +0000

    Added VueRouter

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faStar)
-
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
 
 new Vue({
