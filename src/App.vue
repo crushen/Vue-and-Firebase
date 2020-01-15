f132c4adedfb4bcb2639323853b2189a5853a56a
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-15 10:28:13 +0000

    Added pagination component

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
 
     <hero />
 
+    <pagination />
+
     <section class="posts">
       <div class="container">
         <div class="posts-type">Latest Posts</div>
@@ -59,25 +61,20 @@
         </div>
       </div>
     </section>
-    <nav class="pagination is-rounded  is-centered" role="navigation" aria-label="pagination">
-      <ul class="pagination-list">
-          <li><a class="pagination-previous has-text-weight-bold" href="/">Newer Posts</a></li>
-          <li><a class="pagination-link button has-text-weight-bold is-primary">Page 2 of 5</a></li>
-          <li><a class="pagination-next has-text-weight-bold" href="/page/3/">Older Posts</a></li>
-      </ul>
-    </nav>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/Header';
 import Hero from '@/components/Hero';
+import Pagination from '@/components/Pagination';
 
 export default {
   name: 'app',
   components: {
     AppHeader,
-    Hero
+    Hero,
+    Pagination
   }
 }
 </script>
