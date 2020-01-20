0a887e6f6a67358d57df5662ac8066dbccc1c0b3
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-20 17:40:09 +0000

    Hide or show pages for different users

--- src/router/index.js
+++ src/router/index.js
@@ -7,21 +7,73 @@ import LoginPage from '@/pages/Login';
 import RegisterPage from '@/pages/Register';
 import ProfilePage from '@/pages/Profile';
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'HomePage', component: HomePage },
-  { path: '/about', name: 'AboutPage', component: AboutPage },
-  { path: '/faq', name: 'FaqPage', component: FaqPage },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/register', name: 'RegisterPage', component: RegisterPage },
-  { path: '/users/me', name: 'ProfilePage', component: ProfilePage }
+  { 
+    path: '/', 
+    name: 'HomePage', 
+    component: 
+    HomePage 
+  },
+  { 
+    path: '/about', 
+    name: 'AboutPage', 
+    component: 
+    AboutPage 
+  },
+  { 
+    path: '/faq', 
+    name: 'FaqPage', 
+    component: FaqPage 
+  },
+  { 
+    path: '/login', 
+    name: 'LoginPage', 
+    component: LoginPage,
+    meta: { onlyGuestUser: true }
+  },
+  { 
+    path: '/register', 
+    name: 'RegisterPage', 
+    component: RegisterPage,
+    meta: { onlyGuestUser: true }
+  },
+  { 
+    path: '/users/me', 
+    name: 'ProfilePage', 
+    component: ProfilePage, 
+    meta: { onlyAuthUser: true } 
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const authenticatedUser = firebase.auth().currentUser;
+  if(to.meta.onlyAuthUser) {
+    if(authenticatedUser) {
+      next();
+    } else {
+      // TODO: Navigate to page for non auth user
+      next({name: 'LoginPage'})
+    }
+  } else if(to.meta.onlyGuestUser) {
+    if(authenticatedUser) {
+      next({name: 'HomePage'});
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 })
 
 export default router
