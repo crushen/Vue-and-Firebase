24c480c6859aec006fb2ba269290c7380ce15d4c
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-20 09:27:37 +0000

    Added vue-toasted

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router';
+import router from './router'
 import store from './store'
+import Toasted from 'vue-toasted'
 
 // Font awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -11,6 +12,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faStar)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
+Vue.use(Toasted)
 
 new Vue({
   router,
