1a5ea98878abf3fbc15600058c5dc3fec7e1a53b
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-22 10:22:08 +0000

    Bind form inputs to form data

--- src/pages/ExchangeCreate.vue
+++ src/pages/ExchangeCreate.vue
@@ -6,9 +6,9 @@
           <label class="label">Type</label>
           <div class="control">
             <div class="select">
-              <select>
-                <option>Service</option>
-                <option>Product</option>
+              <select v-model="form.type">
+                <option value="service">Service</option>
+                <option value="product">Product</option>
               </select>
             </div>
           </div>
@@ -16,43 +16,73 @@
         <div class="field">
           <label class="label">Title</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.title"
+              class="input" 
+              type="text" 
+              placeholder="Title">
           </div>
         </div>
         <div class="field">
           <label class="label">Description</label>
           <div class="control">
-            <textarea class="textarea" placeholder="Textarea"></textarea>
+            <textarea 
+              v-model="form.description"
+              class="textarea" 
+              placeholder="Description" />
           </div>
         </div>
         <div class="field">
           <label class="label">Image Link</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.image"
+              class="input" 
+              type="text" 
+              placeholder="Image Link">
           </div>
         </div>
         <div class="field">
-          <label class="label">Value</label>
+          <label class="label">Price</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.price"
+              class="input" 
+              type="number" 
+              placeholder="Price">
           </div>
         </div>
         <div class="field">
           <label class="label">Country</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.country"
+              class="input" 
+              type="text" 
+              placeholder="Country">
           </div>
         </div>
         <div class="field">
           <label class="label">City</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.city"
+              class="input" 
+              type="text" 
+              placeholder="City">
           </div>
         </div>
 
+        <!-- TODO: Provide tags input -->
+
         <div class="field is-grouped">
           <div class="control">
-            <button class="button is-link">Submit</button>
+            <button 
+              @click="createExchange"
+              type="button"
+              class="button is-link">
+              Submit
+            </button>
           </div>
           <div class="control">
             <button class="button is-text">Cancel</button>
@@ -64,6 +94,27 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      form: {
+        type: 'service',
+        title: '',
+        description: '',
+        image: '',
+        price: null,
+        country: '',
+        city: '',
+        tags: []
+      }
+    }
+  },
+  methods: {
+    createExchange() {
+      alert(JSON.stringify(this.form));
+    }
+  }
+}
 </script>
 
 
