9789f1aa3bfdb47b3ff523d21566f93f1cce0c77
Charlotte Rushen <charlotterushen@hotmail.com>
2020-01-22 14:34:52 +0000

    Form validation on Registration page

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -15,10 +15,20 @@
                      type="email"
                      placeholder="Your Email"
                      autocomplete="email">
-              <!-- <div class="form-error">
-                <span class="help is-danger">Email is required</span>
-                <span class="help is-danger">Email address is not valid</span>
-              </div> -->
+              <div 
+                v-if="$v.form.email.$error"
+                class="form-error">
+                <span 
+                  v-if="!$v.form.email.required"
+                  class="help is-danger">
+                  Email is required
+                </span>
+                <span 
+                  v-if="!$v.form.email.email"
+                  class="help is-danger">
+                  Email address is not valid
+                </span>
+              </div>
             </div>
           </div>
           <div class="field">
@@ -27,10 +37,15 @@
                      class="input is-large"
                      type="text"
                      placeholder="Full Name">
-              <!-- <div class="form-error">
-                <span class="help is-danger">Email is required</span>
-                <span class="help is-danger">Email address is not valid</span>
-              </div> -->
+              <div 
+                v-if="$v.form.fullName.$error"
+                class="form-error">
+                <span 
+                  v-if="!$v.form.fullName.required"
+                  class="help is-danger">
+                  Full name is required
+                </span>
+              </div>
             </div>
           </div>
           <div class="field">
@@ -39,10 +54,15 @@
                      class="input is-large"
                      type="text"
                      placeholder="Avatar URL">
-              <!-- <div class="form-error">
-                <span class="help is-danger">Email is required</span>
-                <span class="help is-danger">Email address is not valid</span>
-              </div> -->
+              <div 
+                v-if="$v.form.avatar.$error"
+                class="form-error">
+                <span 
+                  v-if="!$v.form.avatar.required"
+                  class="help is-danger">
+                  Avatar is required
+                </span>
+              </div>
             </div>
           </div>
           <div class="field">
@@ -52,9 +72,15 @@
                      type="password"
                      placeholder="Your Password"
                      autocomplete="current-password">
-              <!-- <div class="form-error">
-                <span class="help is-danger">Password is required</span>
-              </div> -->
+              <div 
+                v-if="$v.form.password.$error"
+                class="form-error">
+                <span 
+                  v-if="!$v.form.password.required"
+                  class="help is-danger">
+                  Password is required
+                </span>
+              </div>
             </div>
           </div>
           <div class="field">
@@ -63,12 +89,19 @@
                      class="input is-large"
                      type="password"
                      placeholder="Password Confirmation">
-              <!-- <div class="form-error">
-                <span class="help is-danger">Password is required</span>
-              </div> -->
+              <div 
+                v-if="$v.form.passwordConfirmation.$error"
+                class="form-error">
+                <span 
+                  v-if="!$v.form.password.sameAsPasword"
+                  class="help is-danger">
+                  Password doesn't match
+                </span>
+              </div>
             </div>
           </div>
-          <button @click="handleRegister"
+          <button @click="onRegister"
+                  :disabled="this.$v.form.$invalid && $v.form.$dirty"
                   type="button"
                   class="button is-block is-info is-large is-fullwidth">Sign Up</button>
         </form>
@@ -83,6 +116,8 @@
 </template>
 
 <script>
+import { required, email, sameAs } from 'vuelidate/lib/validators'
+
 export default {
   data() {
     return {
@@ -95,7 +130,33 @@ export default {
       }
     }
   },
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      fullName: {
+        required
+      },
+      avatar: {
+        required
+      },
+      password: {
+        required
+      },
+      passwordConfirmation: {
+        sameAsPasword: sameAs('password')
+      }
+    }
+  },
   methods: {
+    onRegister() {
+      this.$v.form.$touch()
+      if(!this.$v.form.$invalid) {
+        this.handleRegister()
+      }
+    },
     handleRegister() {
       this.$store.dispatch('auth/signUp', this.form)
         .then(async user => {
@@ -110,7 +171,7 @@ export default {
           this.$router.push('/')
         }).catch(errorMessage => {
           this.$toasted.error(errorMessage, { duration: 3000 })
-        })
+        }) 
     }
   }
 }
