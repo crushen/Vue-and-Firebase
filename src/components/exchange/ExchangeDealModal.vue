5f27f53d581b2171de0c30922c19aa5280f49c99
Charlotte Rushen <charlotterushen@hotmail.com>
2020-02-04 14:06:44 +0000

    Added content for exchange deal modal

--- src/components/exchange/ExchangeDealModal.vue
+++ src/components/exchange/ExchangeDealModal.vue
@@ -10,7 +10,49 @@
         </button>
       </template>
 
-      <h1>Some Deal</h1>
+      <div class="deal">
+        <!-- TODO: replace by actual name -->
+        <div class="deal-highlight">Exchange Owner Offer</div>
+        <div class="deal-wrapper">
+          <!-- TODO: type of an exchange -->
+          <div>Offering service</div>
+          <!-- TODO: title of exchange  -->
+          <div>"Renting book"</div>
+        </div>
+        <div class="deal-highlight">Your Offer</div>
+        <div class="counter-offer">
+          <div class="field">
+            Would you prefer to exchange credit ?
+            <label class="checkbox is-large">
+              <input type="checkbox">
+              Yes
+            </label>
+          </div>
+          <!-- TODO: provide "disabled" class when user is NOT offering credit -->
+          <div class="field">
+            <label class="label">How Much Credit ?</label>
+            <div class="control">
+              <input
+                class="input"
+                type="number"
+                placeholder="40">
+            </div>
+          </div>
+          <!-- TODO: provide "disabled" class when user IS offering credit -->
+          <div class="field">
+            <label class="label">Exchange</label>
+            <div class="control">
+              <div class="select">
+                <select>
+                  <option value="service">Service</option>
+                  <option value="product">Product</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </app-modal>
   </div>
 </template>
@@ -40,3 +82,37 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+  .deal-wrapper {
+    margin-bottom: 10px
+  }
+  .counter-offer, .deal-wrapper {
+    border: 2px solid grey;
+    padding: 10px;
+    margin-top: 10px;
+    &-title {
+      font-size: 21px;
+      margin: 5px 0;
+      font-weight: bold;
+    }
+  }
+  .deal {
+    font-size: 19px;
+    &-highlight {
+      font-size: 19px;
+      font-weight: bold;
+    }
+  }
+  .disabled {
+  &.field {
+    input, textarea, select {
+      pointer-events: none;
+      color: grey;
+    }
+    label {
+      color: grey;
+    }
+  }
+}
+</style> 
